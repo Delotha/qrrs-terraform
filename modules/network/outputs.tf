7350ef93387787bb0f7152dd117fec3e4610bf39
Ryan <ryan@loudfoot.ca>
2025-08-20 23:02:01 -0400

    Fix network outputs

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.qrrs.id  # or whatever your VPC resource is named
+  value       = module.qrrs_vpc.vpc_id
 }
 
 output "public_subnets" {
   description = "List of public subnet IDs"
-  value       = aws_subnet.public[*].id  # or whatever your subnet resource names are
+  value       = module.qrrs_vpc.public_subnets
 }
