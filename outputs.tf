d2e2cc35bdda2afe7b4c6ba28c19c75d289a3f82
Ryan <ryan@loudfoot.ca>
2025-08-21 11:54:57 -0400

    Remove database outputs for now

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,15 @@
-output "db_endpoint" {
-  description = "PostgreSQL connection endpoint from the database module"
-  value       = module.database.db_endpoint
-}
+# # Database outputs
+# output "db_endpoint" {
+#   description = "PostgreSQL connection endpoint from the database module"
+#   value       = module.database.db_endpoint
+# }
 
-output "db_port" {
-  description = "PostgreSQL port from the database module"
-  value       = module.database.db_port
-}
+# output "db_port" {
+#   description = "PostgreSQL port from the database module"
+#   value       = module.database.db_port
+# }
 
-output "db_name" {
-  description = "Database name from the database module"
-  value       = module.database.db_name
-}
+# output "db_name" {
+#   description = "Database name from the database module"
+#   value       = module.database.db_name
+# }
