b4d5b1e37d15a77ae5ee1ccb4ce1ba9db7dc6b72
Ryan <ryan@loudfoot.ca>
2025-08-21 09:04:12 -0400

    Fix database/outputs.tf

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -1,14 +1,14 @@
 output "db_endpoint" {
   description = "Database connection endpoint"
-  value       = aws_db_instance.postgres.endpoint
+  value       = aws_db_instance.database.endpoint
 }
 
 output "db_port" {
   description = "Database port"
-  value       = aws_db_instance.postgres.port
+  value       = aws_db_instance.database.port
 }
 
 output "db_name" {
   description = "Database name"
-  value       = aws_db_instance.postgres.db_name
+  value       = aws_db_instance.database.db_name
 }
