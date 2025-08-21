5aa5aba9e0160d8fcd623b3cd869bd1232cf34e8
Ryan <ryan@loudfoot.ca>
2025-08-21 17:40:58 -0400

    Remove duplicate network variables

--- modules/network/providers/aws/variables.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.qrrs_vpc.vpc_id
-}
-
-output "public_subnets" {
-  description = "List of public subnet IDs"
-  value       = module.qrrs_vpc.public_subnets
-}
